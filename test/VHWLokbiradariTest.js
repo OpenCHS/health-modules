7f604ad6c7f6061531c50b203809fb3a4ebc8d28
mihirk <mihir.khatwani@gmail.com>
2016-09-22 17:13:39 +0530

    Fixing ES6 issues

--- test/VHWLokbiradariTest.js
+++ test/VHWLokbiradariTest.js
@@ -2,18 +2,18 @@ var expect = require('chai').expect;
 var decision = require('../modules/vhw/decision');
 var RuleContext = require('./RuleContext');
 
-describe('Make Decision', function() {
-    it('Regression for all diseases, to ensure there are no exceptions and error messages', function() {
+describe('Make Decision', function () {
+    it('Regression for all diseases, to ensure there are no exceptions and error messages', function () {
         var ruleContext = new RuleContext();
 
-        Object.keys(decision.treatmentByComplaintAndCode).forEach(function(complaint) {
-            decision.weightRangesToCode.forEach(function(weightRangeToCode) {
-                ["Male", "Female"].forEach(function(gender) {
+        Object.keys(decision.treatmentByComplaintAndCode).forEach(function (complaint) {
+            decision.weightRangesToCode.forEach(function (weightRangeToCode) {
+                ["Male", "Female"].forEach(function (gender) {
                     ruleContext.set("Complaint", [complaint]);
                     ruleContext.set("Weight", weightRangeToCode.start);
                     ruleContext.set("Sex", gender);
                     ruleContext.set("Age", 10);
-                    console.log(`##### ${complaint}, ${weightRangeToCode.start}, ${gender}  ######`);
+                    console.log("##### {complaint}, {weightRangeToCode.start}, {gender}  ######".replace("${complaint}", complaint).replace("{weightRangeToCode.start}", weightRangeToCode.start).replace("{gender}", gender));
                     if (decision.validate(ruleContext).passed) {
                         var decisions = decision.getDecision(ruleContext);
                         expect(decisions.length).to.equal(1);
@@ -23,7 +23,7 @@ describe('Make Decision', function() {
         });
     });
 
-    it('Validate', function() {
+    it('Validate', function () {
         var complaintConceptName = "Complaint";
         var validationResult = decision.validate(new RuleContext().set(complaintConceptName, ["Pregnancy"]).set("Sex", "Male").set("Age", 25).set("Weight", 40));
         expect(validationResult.passed).to.equal(false, validationResult.message);
@@ -38,13 +38,14 @@ describe('Make Decision', function() {
         expect(validationResult.passed).to.equal(false, validationResult.message);
     });
 
-    it('Complaint which allows for prescription', () => {
+    it('Complaint which allows for prescription', function() {
         var decisions = decision.getDecision(new RuleContext().set("Complaint", ["Cold"]).set("Sex", "Male").set("Age", 25).set("Weight", 40));
-        expect(decisions[0].value).to.not.equal(undefined);
-        expect(decisions[0].alert).to.equal(undefined);
-    });
-    
-    it('Do not give any medicine for chloroquin resistant malaria to women between 16-40', function() {
+    expect(decisions[0].value).to.not.equal(undefined);
+    expect(decisions[0].alert).to.equal(undefined);
+})
+    ;
+
+    it('Do not give any medicine for chloroquin resistant malaria to women between 16-40', function () {
         var decisions = decision.getDecision(new RuleContext().set("Complaint", ["Chloroquine Resistant Malaria"]).set("Sex", "Female").set("Age", 25).set("Weight", 40));
         expect(decisions[0].value).to.equal("");
         expect(decisions[0].alert).to.not.equal(undefined);
--- test/testHelper.js
+++
@@ -1,8 +0,0 @@
-require('babel-core/register')({
-    ignore: function (filename) {
-        if (filename.indexOf("node_modules") === -1) {
-            return false;
-        }
-        return true;
-    }
-});
