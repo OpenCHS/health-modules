a0ab6cb62809ab4e8c546148a6097fc152fd4254
pradiptak <nubrapika@gmail.com>
2017-03-03 15:52:25 +0530

    HRP logic for Moderate or Severe Anemia

--- deployables/programEncounter.js
+++ deployables/programEncounter.js
@@ -1,21 +1,32 @@
 var anemiaCheck = 'Hb';
-var decision;
+var decisions = [];
+var decision = {};
 
 const getDecision = function (programEncounter) {
 
     var observations = programEncounter.observations;
     for (var i = 0; i < programEncounter.observations.length; i++) {
         if (observations[i].concept.name === anemiaCheck) {
-            if (observations[i].valueJSON.answer <= 7)
-                decision = "Severe. Please refer to FRU" ;
-            else if (observations[i].valueJSON.answer > 7 || observations[i].valueJSON.answer <= 11 )
-                decision = "Moderately Severe. Needs treatment" ;
-            else decision = "Normal"
+            if (observations[i].valueJSON.answer < 7){
+                decision = {riskFactorType: 'Moderate and Severe Anemia', message: "Severe Anemia. Please refer to FRU"};
+                decisions.push(decision);
 
+            }
+            else if (observations[i].valueJSON.answer >= 7 || observations[i].valueJSON.answer <= 11 ){
+                decision = {riskFactorType: 'Moderate and Severe Anemia', message: "Moderately Severe Anemia. Needs treatment"};
+                decisions.push(decision);
+            }
+            else {
+                decision = {riskFactorType: 'Moderate and Severe Anemia', message: "Hb normal. Please proceed with IFA tablets"};
+                decisions.push(decision);
+            }
         }
-
+        if (observations[i].concept.name == 'Convulsions') {
+            decision = {riskFactorType: 'Convulsions', message: "Continue to monitor. Refer to MO if condition is not under control before 8th month"};
+            decisions.push(decision);
+        }
     }
-    return decision;
+    return decisions;
 };
 
 module.exports = {
--- test/HighRiskCheckTest.js
+++ test/HighRiskCheckTest.js
@@ -2,6 +2,30 @@ var expect = require('chai').expect;
 var getDecision = require('../deployables/programEncounter');
 
 describe('Make Decision', function () {
+
+    it('Check to show right risk factor based on symptoms', function () {
+        var observations = [];
+        observation = {
+            concept: {name: "Hb"},
+            valueJSON: {answer: 4}
+        };
+        observations.push(observation);
+        observation = {
+            concept: {name: "Convulsions"},
+            valueJSON: {answer: 'Yes'}
+        };
+        observations.push(observation);
+        var progEncounter = {};
+        progEncounter.observations = observations;
+        var decisions = getDecision.getDecision(progEncounter);
+        var riskFactorTypes = decisions.map(function(decision) {
+            return decision.riskFactorType;
+        });
+        expect(riskFactorTypes.indexOf("Moderate and Severe Anemia")).is.not.equal(-1);
+        expect(riskFactorTypes.indexOf("Convulsions")).is.not.equal(-1);
+        console.log(riskFactorTypes);
+    });
+
     it('Check to show right suggestion for Anemia based on Hb result', function () {
         var observations = [];
         var observation = {
@@ -36,4 +60,5 @@ describe('Make Decision', function () {
         expect(decision).is.equal('Severe. Please refer to FRU');
         console.log(decision);
     });
+
 });
