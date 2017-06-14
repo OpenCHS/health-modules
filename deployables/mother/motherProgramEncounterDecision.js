b65d552a32c3acf764cd1d8e772620707f4eed64
Arjun <arjunk@thoughtworks.com>
2017-06-14 17:55:17 +0530

    Adding rule for weight gain per month

--- deployables/mother/motherProgramEncounterDecision.js
+++ deployables/mother/motherProgramEncounterDecision.js
@@ -14,11 +14,32 @@ module.exports.getDecisions = function (programEncounter, today) {
     const previousWeightRecordDate = previousEncounter.encounterDateTime;
     const weightGain = currentWeight - previousWeight;
 
-    const daysBetweenCurrentAndPreviousWeightRecord = C.getDays(currentWeightRecordDate, previousWeightRecordDate);
+    const monthsBetweenCurrentAndPreviousWeightRecord = C.getDays(currentWeightRecordDate, previousWeightRecordDate)/30;
 
+    if(monthsBetweenCurrentAndPreviousWeightRecord >= 1){
+
+        function addComplication(conceptName) {
+            console.log('(MotherProgramEncounterDecision) Adding if not exists to preg complications: ' + conceptName);
+            var pregnancyComplications = C.findValue(decisions, 'Pregnancy Complications');
+            if (pregnancyComplications === undefined){
+                pregnancyComplications = [];
+                decisions.push({name: 'Pregnancy Complications', value: pregnancyComplications})
+            }
+            pregnancyComplications.push(conceptName);
+        }
+
+        const weightGainPerMonth = weightGain / monthsBetweenCurrentAndPreviousWeightRecord;
+        if(weightGainPerMonth < 1){
+            addComplication('Weight Gain Per Month less than 1kg');
+        }else if(weightGainPerMonth > 1.5){
+            addComplication('Weight Gain Per Month more than 1.5kg');
+        }
+    }
 
     return decisions;
 };
+
+
 
 module.exports.getNextScheduledVisits = function (programEncounter, today) {
     return programDecision.getNextScheduledVisits(programEncounter.programEnrolment, today, programEncounter);
